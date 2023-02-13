054d20f782154468cfdf7ef0c6dd3b138078c29b
Eric x z pan <simson2010@hotmail.com>
2023-02-13 23:29:52 +0800

    add back and forward button

--- MyBrowser.py
+++ MyBrowser.py
@@ -1,50 +1,42 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QVBoxLayout, QWidget
-from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextBrowser, QLineEdit, QVBoxLayout, QHBoxLayout, QPushButton
 from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 class MyBrowser(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("MyBrowser")
 
-        # 地址栏
-        self.url_line_edit = QLineEdit(self)
-        self.url_line_edit.returnPressed.connect(self.load_url)
+        self.setWindowTitle("Web Browser")
+        self.setGeometry(100,100,800,600)
 
-        # 浏览器视图
         self.web_engine_view = QWebEngineView(self)
-        self.web_engine_view.urlChanged.connect(self.update_url_line_edit)
+        self.line_edit = QLineEdit(self)
+        self.line_edit.returnPressed.connect(self.load_url)
+        self.back_button = QPushButton("Back", self)
+        self.back_button.clicked.connect(self.web_engine_view.back)
+        self.forward_button = QPushButton("Forward", self)
+        self.forward_button.clicked.connect(self.web_engine_view.forward)
 
-        #创建新窗口
-        self.web_engine_view.createWindow(QWebEnginePage.WebBrowserWindow).connect(new_window)
-
-        # 垂直布局
         layout = QVBoxLayout()
-        layout.addWidget(self.url_line_edit)
+        buttons_layout = QHBoxLayout()
+        buttons_layout.addWidget(self.back_button)
+        buttons_layout.addWidget(self.forward_button)
+        layout.addLayout(buttons_layout)
+        layout.addWidget(self.line_edit)
         layout.addWidget(self.web_engine_view)
 
-        # 中央窗口部件
-        central_widget = QWidget(self)
+        central_widget = QMainWindow(self)
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
     def load_url(self):
-        url = self.url_line_edit.text()
-        if not url.startswith("http://") and not url.startswith("https://"):
+        url = self.line_edit.text()
+        if not url.startswith("http"):
             url = "http://" + url
-        self.web_engine_view.load(QUrl(url))
-
-    def update_url_line_edit(self, url):
-        self.url_line_edit.setText(url.toString())
-
-    def new_window(self, url):
-        browser = MyBrowser()
-        browser.web_engine_view.load(QUrl(url))
-        browser.show()
+        self.web_engine_view.setUrl(QUrl(url))
 
 app = QApplication(sys.argv)
 browser = MyBrowser()
 browser.show()
-browser.load_url()
 sys.exit(app.exec_())
