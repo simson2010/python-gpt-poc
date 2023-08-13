8886ed05df8882ca9a93020b38e0a80056f8e390
Eric Pan <simson2010@hotmail.com>
2023-08-13 22:38:37 +0800

    Update app.py
    add func 
    invoke func 
    print result

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 data = [['a','b','c'], ['1','2','3','4']]
-df = pd.dataframe(data, columns=['name','value','key','values'])
 
+def createDF()
+df = pd.DataFrame(data, columns=['name','value','key','values'])
+
+df = createDF()
 print(df)
