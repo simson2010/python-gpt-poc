833a1a31031a59e15ae1e0c204839e1ed48d3bcb
Eric X Z Pan <ericpoon2001@gmail.com>
2023-08-14 16:31:13 +0800

    [update] fix wrong suggestion from PR-agent

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 data = [['a','b','c'], ['1','2','3','4']]
 
 def createDataFrame():
-  df = pd.DataFrame(data, columns=['name', 'value', 'key', 'values']):
+  df = pd.DataFrame(data, columns=['name', 'value', 'key', 'values'])
   return df
 
 df = createDataFrame()
