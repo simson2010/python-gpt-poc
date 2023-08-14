fdd145b864f1c24e5db4769dcdaeccb0b3ecf00c
Eric Pan <simson2010@hotmail.com>
2023-08-14 15:49:07 +0800

    Update app.py
    add missing colon(:)
    fix indentation of line 6

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 data = [['a','b','c'], ['1','2','3','4']]
 
-def createDF()
-df = pd.DataFrame(data, columns=['name','value','key','values'])
+def createDataFrame():
+  df = pd.DataFrame(data, columns=['name', 'value', 'key', 'values'])
+  return df
 
 df = createDF()
 print(df)
